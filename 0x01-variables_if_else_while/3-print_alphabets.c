e5dabe6f3bd6e3c10f7c48f7ad2df1b2fc2cc7d1
mthethonhla <mahlatsenhl@gmail.com>
2023-09-08 11:40:22 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,13 +6,17 @@
  *
  */
 
-int main(void){
+int main(void)
+{
 	char lower;
 	for(lower = 'a'; lower <= 'z'; lower++)
-	putchar(lower);
-
+	{
+		putchar(lower);
+	}
 	for(lower = 'A'; lower <= 'Z'; lower++)
-        putchar(lower);
+	{
+		putchar(lower);
+	}
 	putchar('\n');
 	return (0);
 }
