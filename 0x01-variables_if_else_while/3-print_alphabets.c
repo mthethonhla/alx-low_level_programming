de4cc015e0ff5a12ca78bcb8eba728adddb85494
mthethonhla <mahlatsenhl@gmail.com>
2023-09-08 11:45:18 -0700

    3-print_alphabets betty2 errors

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	char lower;
-	for(lower = 'a'; lower <= 'z'; lower++)
+	for (lower = 'a'; lower <= 'z'; lower++)
 	{
 		putchar(lower);
 	}
-	for(lower = 'A'; lower <= 'Z'; lower++)
+	for (lower = 'A'; lower <= 'Z'; lower++)
 	{
 		putchar(lower);
 	}
