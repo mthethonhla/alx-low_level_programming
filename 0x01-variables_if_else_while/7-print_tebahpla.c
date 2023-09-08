a1a585f975174d28d160d6d68b43f0479dd9f6f4
mthethonhla <mahlatsenhl@gmail.com>
2023-09-08 13:33:29 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - program that prints the lowercase alphabet in reverse
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n = 122;
+
+	while(n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
