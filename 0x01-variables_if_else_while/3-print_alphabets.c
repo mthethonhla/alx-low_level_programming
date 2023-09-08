15688f94eda522a6cf8afe142d52238c7b5d33e2
mthethonhla <mahlatsenhl@gmail.com>
2023-09-08 11:30:34 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void){
+	char lower;
+	for(lower = 'a'; lower <= 'z'; lower++)
+	putchar(lower);
+
+	for(lower = 'A'; lower <= 'Z'; lower++)
+        putchar(lower);
+	putchar('\n');
+	return (0);
+}
