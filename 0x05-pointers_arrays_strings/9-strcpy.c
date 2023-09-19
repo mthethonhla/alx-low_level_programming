d2a7825d38b9b671b86d10c117bbc110cdfee799
mthethonhla <mahlatsenhl@gmail.com>
2023-09-19 08:06:50 -0700

    fixed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * char *_strcpy - function that copies the string
+ * *_strcpy - function that copies the string
  * pointed to by src
- * @dest: destionatio
+ * @dest: destionation
  * @src: from
  * return: string
  *
@@ -13,7 +13,7 @@ char *_strcpy(char *dest, char *src)
 int i;
 for (i = 0; src[i] != '\0'; i++)
 {
-dest[i] = src [i];
+dest[i] = src[i];
 }
 dest[i++] = '\0';
 return (dest);
