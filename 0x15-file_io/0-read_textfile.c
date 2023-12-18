7b033bd069d59be7385ca481839f0959bb63c069
mthethonhla <nhlamthetho@outlook.com>
2023-12-18 02:48:56 -0800

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints it to the POSIX standard output.
+ * @filename: the filename.
+ * @letters: letters is the number of letters it should read and print
+ *
+ * Return: returns the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	ssize_t newd, newr;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+
+	newd = read(file, buffer, letters);
+	newr = write(STDOUT_FILENO, buffer, newd);
+
+	close(file);
+
+	free(buffer);
+
+	return (newr);
+}
